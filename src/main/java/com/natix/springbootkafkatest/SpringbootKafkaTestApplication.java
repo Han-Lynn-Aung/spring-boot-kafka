e2d80fa93416db22e3484fecd285e580e49fe695
Han-Lynn-Aung <hanlynn.aung@cdsg.com.mm>
2024-01-26 16:32:53 +0630

    spring boot kafka config

---
+++ src/main/java/com/natix/springbootkafkatest/SpringbootKafkaTestApplication.java
@@ -0,0 +1,13 @@
+package com.natix.springbootkafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natix/springbootkafkatest/SpringbootKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natix.springbootkafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
